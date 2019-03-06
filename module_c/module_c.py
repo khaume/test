9ef2641aa8d731198a08ce3293af62423c26f6cc
Kaspar Haume <KAHAU@orsted.dk>
2019-03-06 12:14:37 +0100

    module_c commit 2

--- module_c/module_c.py
+++ module_c/module_c.py
@@ -1,7 +1,10 @@
 import test.module_a as module_a
+from .utils import SOME_STR, VAR_1
 
 print('in module_c')
 
-module_a.my_fun(1, 2)
+module_a.my_fun(1, VAR_1)
 
 print('just executed module_a')
+print('string is: ', SOME_STR)
+
