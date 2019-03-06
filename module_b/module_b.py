a6171de186719c48e7533c9db474f074a4fe7c32
Kaspar Haume <KAHAU@orsted.dk>
2019-03-06 13:03:22 +0100

    module_b commit. more docstring

--- module_b/module_b.py
+++ module_b/module_b.py
@@ -2,9 +2,9 @@ def other_fun(var1: int, var2: int) -> str:
     """
     Docstring
 
-    :param var1:
-    :param var2:
-    :return:
+    :param var1: A number
+    :param var2: Another number
+    :return:     A string of the numbers
     """
     print(f'called with {var1} and {var2}')
 
