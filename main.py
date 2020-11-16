a596f24ba1fdd470e49c72f861861ae3f877dffd
KAHAU <kahau@orsted.dk>
2020-11-16 20:52:47 +0100

    small change to main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     print('in the main method..')
 
     # comment in main
-    result = my_fun(10, 5)
+    result = my_fun(20, 5)
 
     # comment again
     print(f'The result was {result}')
